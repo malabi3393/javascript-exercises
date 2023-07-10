fe5cc36939261d5f854c332a36c6a8a4f799f07a
Mariam <mariam.alabi@outlook.com>
2023-07-10 13:32:59 -0400

    Palindrome exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = removePunc(str);
+    var arr = str.split('');
+    var reverseArr = [];
+    for (var i = arr.length - 1; i >= 0; i --) {
+        reverseArr.push(arr[i]);
+    };
+
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== reverseArr[i]) {
+            return false;
+        }
+
+    }
+    return true;
+
 
 };
+
+function removePunc(str) {
+    str = str.trim();
+    str = str.replaceAll(/\s/g,'');
+    str = str.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    str = str.toLowerCase();
+
+    return str;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
