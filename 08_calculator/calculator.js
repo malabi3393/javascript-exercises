bb6e48441f9f59d4da5d1ca5aef9a621d4c326aa
Mariam <mariam.alabi@outlook.com>
2023-07-10 12:52:21 -0400

    complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum = arr.reduce(function(sum, current) {
+    return sum + current;
+  }, 0);
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(...elements) {
+  let product = elements.reduce(function(product, current) {
+    return product * current;
+  }, 1);
+  return product;
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let fact = 1;
+  for (let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 	
 };
 
