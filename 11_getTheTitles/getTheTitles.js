ea68ffed261c9398766ae0e9c442d705fc826385
Mariam <mariam.alabi@outlook.com>
2023-07-10 14:03:18 -0400

    complete add titles exercise:

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,37 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    bookNames = [];
+
+    for (titles in books) {
+        bookNames.push(books[titles].title);
+    }
+
+    return bookNames;
+
 
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ];
+
+// var bookNames = [];
+
+// for (titles in books) {
+//     console.log(books[titles].title);
+//     bookNames.push(titles.title);
+// }
+
+// console.log(bookNames);
+
+// console.log(books[0].author);
