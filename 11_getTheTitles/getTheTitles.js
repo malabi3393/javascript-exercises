3febdc06b7dd899a666bc81e4c55006fc35aa2d9
Mariam <mariam.alabi@outlook.com>
2023-07-10 14:13:44 -0400

    code clean up

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -12,26 +12,3 @@ const getTheTitles = function(books) {
 
 // Do not edit below this line
 module.exports = getTheTitles;
-
-
-// const books = [
-//     {
-//       title: 'Book',
-//       author: 'Name'
-//     },
-//     {
-//       title: 'Book2',
-//       author: 'Name2'
-//     }
-//   ];
-
-// var bookNames = [];
-
-// for (titles in books) {
-//     console.log(books[titles].title);
-//     bookNames.push(titles.title);
-// }
-
-// console.log(bookNames);
-
-// console.log(books[0].author);
