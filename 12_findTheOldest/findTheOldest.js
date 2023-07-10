6e096f983e2cb26733468c94d620134ea97b16d4
Mariam <mariam.alabi@outlook.com>
2023-07-10 14:12:53 -0400

    complete find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    for (person in people) {
+        if (people[person].yearOfDeath === undefined) {
+            people[person].yearOfDeath = (new Date()).getFullYear();
+        }
+    }
+
+    let oldest = people.sort((a,b) => a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1);
+
+    return oldest[0];
+
+
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
